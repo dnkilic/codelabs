8ea4a7e61c4f0b17e51c055eb42700d7a8f3f43f
Dogan Kilic <dnkilic@outlook.com>
2019-03-10 16:58:30 +0700

    Write Your First Flutter App Codelab, part 2 completed

--- first_flutter_app/first_flutter_app/README.md
+++ first_flutter_app/first_flutter_app/README.md
@@ -1,16 +1,9 @@
-# first_flutter_app
+# My First Flutter App
 
-Write Your First Flutter App Codelab, part 1 
+# Write Your First Flutter App Codelab, part 1 and part 2
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.io/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.io/docs/cookbook)
-
-For help getting started with Flutter, view our 
-[online documentation](https://flutter.io/docs), which offers tutorials, 
-samples, guidance on mobile development, and a full API reference.
+<p p align="middle">
+  <img src="/screenshots/part1.png" width="100" />
+  <img src="/screenshots/part2-1.png" width="100" /> 
+  <img src="/screenshots/part2-2.png" width="100" />
+</p>
--- first_flutter_app/first_flutter_app/lib/random_words.dart
+++ first_flutter_app/first_flutter_app/lib/random_words.dart
@@ -3,14 +3,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class _RandomWordsState extends State<RandomWords> {
-  final List<WordPair> _suggestions = <WordPair>[];
+  final List<WordPair> _suggestions = List<WordPair>();
   final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
+  final Set<WordPair> _savedSuggestions = Set<WordPair>();
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       // Add from here...
       appBar: new AppBar(
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.list),
+            onPressed: () {
+              _navigateToFavorited();
+            },
+          )
+        ],
         title: new Text('Startup Name Generator'),
       ),
       body: _buildSuggestions(),
@@ -35,12 +44,50 @@ class _RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final bool _alreadySaved = _savedSuggestions.contains(pair);
+
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: Icon(
+        _alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: _alreadySaved ? Colors.pink : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (_alreadySaved) {
+            _savedSuggestions.remove(pair);
+          } else {
+            _savedSuggestions.add(pair);
+          }
+        });
+      },
     );
+  }
+
+  void _navigateToFavorited() {
+    Navigator.of(context)
+        .push(MaterialPageRoute<void>(builder: (BuildContext _context) {
+      final Iterable<ListTile> tiles = _savedSuggestions.map((WordPair pair) {
+        return ListTile(
+            title: Text(
+          pair.asPascalCase,
+          style: _biggerFont,
+        ));
+      });
+
+      final List<Widget> divided =
+          ListTile.divideTiles(tiles: tiles, context: _context).toList();
+
+      return new Scaffold(
+        appBar: new AppBar(
+          title: const Text('Saved Suggestions'),
+        ),
+        body: new ListView(children: divided),
+      );
+    }));
   }
 }
 
