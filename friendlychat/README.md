120e2113b99f5b93173509f79d3ddc6ef2ec8ece
Dogan Kilic <dnkilic@outlook.com>
2019-03-24 21:53:25 +0700

    Finalize Building Beautiful UIs with Flutter Codelab

--- friendlychat/README.md
+++ friendlychat/README.md
@@ -1,16 +1,47 @@
-# friendlychat
+# Codelab: Building Beautiful UIs with Flutter
 
-Building Beautiful UIs with Flutter Codelab
+Sample project to learn basics of Flutter.
 
 ## Getting Started
 
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.io/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.io/docs/cookbook)
-
-For help getting started with Flutter, view our 
-[online documentation](https://flutter.io/docs), which offers tutorials, 
-samples, guidance on mobile development, and a full API reference.
+## Outcomes
+* To add app bar to application we can use AppBar widget inside Scaffold widget
+* Stateless widgets should override build method
+* Scaffold and AppBar widgets are specific to MaterialApp widget
+* TextField is a stateful widget.
+* In flutter if you want to present stateful data you should encapsulate it inside State object
+* Stateful widgets should override create state method. State class should override build method
+* Using new keyword to create objects is optional
+* We can use TextEditingController to manage the text field interactions. TextField have controller attribute.
+* InputDecoration can be used inside TextField to show hint
+* TextEditingController has clear() method to clear text inside TextField
+* Prefixing an identifier with _ marks it as private.
+* Row and Column can be used to align widgets horizontally or vertically.
+* Row widget has children attribute which can take more than one children.
+* Flexible widget automatically size the text field to use remaining space that isn used by button
+* We can create icon buttons by using IconButton widget
+* Icons inherit their color, opacity and size from IconTheme and us its data property to specify ThemeData object of current theme.
+* CrossAxisAlignment.start can be used inside Row to position children relative to parent widgets
+* Empty array initialization ; final List<ChatMessage> _messages = <ChatMessage>[];
+* Only synchronous operations should be performed in setState method.
+* Theme.of(context) can be used to access theme properties
+* Divider Widget can be use to separate column or row items
+* ListView.builder can be used to populate list view
+* Container is useful when defining background images, padding, margin and other common layout details
+* reverse attribute of ListView grant ListView to show its elements bottom to top
+* Animations in Flutter are encapsulated as Animation objects that contain a typed value and a status(forward, backward, completed and dismissed)
+* AnimationController can be use to specify how the animation should run(duration and playback direction).
+* When creation an AnimationController we must pass it as vsync argument.
+* vsyn argument prevent animations that are offscreen from consuming unnecessary resources.
+* In Dart, a mixin allows a class body to be reused in multiple class hierarchies
+* Duration can be used to set durations
+* SizeTransition class provides an animation effect where the width or height of its child is multiplied by a given size factor value.
+* Its good practice to dispose the animations
+* For each loop; for (ChatMessage message in _messages)
+* Declaring boolean with Dart; bool isComposing = false;
+* setState should be used to access State properties
+* when onPressed button set to null button color automatically use Theme.of(context).disabledColor
+* Expanded widget can be used to wrap text
+* TargetPlatform.iOS and TargetPlatform.android can be used to specify different behaviours for different OS
+* CupertinoButton can be used to add iOS style buttons
+* Sample apps; https://github.com/flutter/flutter/tree/master/examples
--- friendlychat/lib/main.dart
+++ friendlychat/lib/main.dart
@@ -1,40 +1,181 @@
 // This sample shows adding an action to an [AppBar] that opens a shopping cart.
 
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
+
+final ThemeData kIOSTheme = ThemeData(
+    primarySwatch: Colors.orange,
+    primaryColor: Colors.grey[100],
+    primaryColorBrightness: Brightness.light);
+
+final kDefaultTheme = ThemeData(
+    primarySwatch: Colors.purple, accentColor: Colors.orangeAccent[400]);
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Friendlychat',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: ChatScreen());
+      title: 'Friendlychat',
+      home: ChatScreen(),
+      theme: defaultTargetPlatform == TargetPlatform.iOS
+          ? kIOSTheme
+          : kDefaultTheme,
+    );
   }
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
+  final List<ChatMessage> _messages = <ChatMessage>[];
+  final TextEditingController _textController = TextEditingController();
+  bool _isComposing = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
-        children: <Widget>[
-          TextField(
-            decoration: InputDecoration(
-                hintText: 'Send a message',
-                hintStyle: TextStyle(color: Colors.grey[600])),
-            maxLines: 1,
-          ),
+        appBar: AppBar(
+          elevation:
+              Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+          title: Text('Friendlychat'),
+        ),
+        body: Container(
+          decoration: Theme.of(context).platform == TargetPlatform.iOS
+              ? BoxDecoration(
+                  border: Border(top: BorderSide(color: Colors.grey[400])))
+              : null,
+          child: Column(children: <Widget>[
+            Flexible(
+              child: ListView.builder(
+                  padding: EdgeInsets.all(8.0),
+                  reverse: true,
+                  itemCount: _messages.length,
+                  itemBuilder: (_, int index) => _messages[index]),
+            ),
+            Divider(height: 1.0),
+            Container(
+              decoration: BoxDecoration(color: Theme.of(context).cardColor),
+              child: IconTheme(
+                  child: _buildTextComposer(),
+                  data: IconThemeData(color: Theme.of(context).accentColor)),
+            )
+          ]),
+        ));
+  }
 
-          IconButton(icon: Icon(Icons.send), onPressed: () { print(""); })
+  Widget _buildTextComposer() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Row(
+        children: <Widget>[
+          Flexible(
+            child: TextField(
+              onChanged: (String text) {
+                setState(() {
+                  _isComposing = text.length > 0;
+                });
+              },
+              controller: _textController,
+              onSubmitted: _handleSubmitted,
+              decoration: InputDecoration.collapsed(hintText: "Send a message"),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 4.0),
+            child: Theme.of(context).platform == TargetPlatform.iOS
+                ? CupertinoButton(
+                    child: Text('Send'),
+                    onPressed: _isComposing
+                        ? () => _handleSubmitted(_textController.text)
+                        : null,
+                  )
+                : IconButton(
+                    icon: Icon(Icons.send),
+                    onPressed: _isComposing
+                        ? () => _handleSubmitted(_textController.text)
+                        : null,
+                  ),
+          )
         ],
       ),
-      appBar: AppBar(
-        title: Text('Friendlychat'),
+    );
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+    setState(() {
+      _isComposing = false;
+    });
+
+    ChatMessage message = ChatMessage(
+      text: text,
+      animationController: AnimationController(
+          duration: Duration(milliseconds: 700), vsync: this),
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
+    message.animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    for (ChatMessage message in _messages) {
+      message.animationController.dispose();
+    }
+    super.dispose();
+  }
+}
+
+const String _name = "Dogan Kilic";
+
+class ChatMessage extends StatelessWidget {
+  const ChatMessage({Key key, this.text, this.animationController})
+      : super(key: key);
+  final AnimationController animationController;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizeTransition(
+      sizeFactor:
+          CurvedAnimation(parent: animationController, curve: Curves.easeOut),
+      axisAlignment: 0.0,
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 10.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.only(right: 16.0),
+              child: CircleAvatar(
+                child: Text(_name[0]),
+              ),
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    _name,
+                    style: Theme.of(context).textTheme.subhead,
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 5.0),
+                    child: Text(text),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
